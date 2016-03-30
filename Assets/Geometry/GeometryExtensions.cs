d213a7eaa773c29acc6b77ae91aa19335de67a51
0Entropy <impression.cs@gmail.com>
2016-03-31 01:39:02 +0800

    Shape2D Add Mesh

---
+++ Assets/Geometry/GeometryExtensions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Geometry
+{
+
+    public static class NewBehaviourScript
+    {
+
+        public static void AddMesh(this Shape2D shape, Mesh mesh)
+        {
+            for (int i = 0; i < mesh.triangles.Length; i += 3)
+            {
+
+                var p0 = new Point2D(mesh.vertices[mesh.triangles[i]]);//, mesh.name + "-" + mesh.triangles[i]);
+                var p1 = new Point2D(mesh.vertices[mesh.triangles[i + 1]]);//, mesh.name + "-" + mesh.triangles[i+1]);
+                var p2 = new Point2D(mesh.vertices[mesh.triangles[i + 2]]);//, mesh.name + "-" + mesh.triangles[i+2]);
+                
+                var face = shape.AddPoints(p0, p1, p2);
+            }
+        }
+    }
+}
