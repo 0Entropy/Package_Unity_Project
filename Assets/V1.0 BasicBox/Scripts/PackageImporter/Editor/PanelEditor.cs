ac4711185b8c594bea28375092d24310bfb3763b
0Entropy <impression.cs@gmail.com>
2016-04-01 10:51:42 +0800

    Border Line Hover

--- Assets/V1.0 BasicBox/Scripts/PackageImporter/Editor/PanelEditor.cs
+++ Assets/V1.0 BasicBox/Scripts/PackageImporter/Editor/PanelEditor.cs
@@ -9,7 +9,7 @@ using Geometry;
 public class PanelEditor : Editor
 {
 
-    //Shape2D Shape;
+   
 
     public override void OnInspectorGUI()
     {
@@ -21,10 +21,52 @@ public class PanelEditor : Editor
     {
         if (target == null)
             return;
+        
+        if(keyPoints == null)
+        {
+            keyPoints = new List<Vector3>(ImPanel.BorderArray);
+        }
 
 
 
-        DrawLine(ImPanel.Shape.OutlinePoints.Select(p => (Vector2)p.Position), Color.red);
+        Handles.matrix = ImPanel.transform.localToWorldMatrix;
+
+        for (int i=0; i < keyPoints.Count; i++)
+        {
+            Handles.color = nearestLine == i ? Color.green : Color.white;
+            DrawSegment(i);
+        }
+
+        //Handles.DrawSolidRectangleWithOutline(ImPanel.BorderArray, new Color(0.2f, 0.2f, 0.2f, 0.2f), new Color(0.0f, 0.4f, 0.6f));
+
+        //Handles.color = new Color(0, 0.5f, 0.8f);
+
+        foreach(var p in ImPanel.BorderArray)
+        {
+            Handles.CubeCap(0, p, Quaternion.identity, HandleUtility.GetHandleSize(p) * 0.08f);
+        }
+
+        //Debug.Log(string.Format("width : {0}, height : {1}", ImPanel.Width, ImPanel.Height));
+
+        worldToLocal = ImPanel.transform.worldToLocalMatrix;
+        inverseRotation = Quaternion.Inverse(ImPanel.transform.rotation) * Camera.current.transform.rotation;
+        
+        screenMousePosition = new Vector3(e.mousePosition.x, Camera.current.pixelHeight - e.mousePosition.y);
+        var plane = new Plane(-ImPanel.transform.forward, ImPanel.transform.position);
+        var ray = Camera.current.ScreenPointToRay(screenMousePosition);
+        float hit;
+        if (plane.Raycast(ray, out hit))
+            mousePosition = worldToLocal.MultiplyPoint(ray.GetPoint(hit));
+        else
+            return;
+
+        //Update nearest line and split position
+        nearestLine = NearestLine(out nearestPosition);
+
+        var index = -1;
+        TryHoverSegment(out index);
+        //e.Use();
+        HandleUtility.Repaint();
     }
 
     public PanelImporter ImPanel
@@ -52,4 +94,112 @@ public class PanelEditor : Editor
         }
     }
 
+    const float clickRadius = 0.12f;
+
+    List<Vector3> keyPoints;
+
+    Matrix4x4 worldToLocal;
+    Quaternion inverseRotation;
+
+    Vector3 screenMousePosition;
+
+    Vector3 clickPosition;
+    Vector3 mousePosition;
+
+    int nearestLine;
+    List<int> selectedIndices = new List<int>();
+    Vector3 nearestPosition;
+
+    MouseCursor mouseCorsor;
+
+    Event e
+    {
+        get { return Event.current; }
+    }
+
+    void DrawCircle(Vector3 position, float size)
+    {
+        Handles.CircleCap(0, position, inverseRotation, HandleUtility.GetHandleSize(position) * size);
+    }
+
+    void DrawSegment(int index)
+    {
+        var from = keyPoints[index];
+        var to = keyPoints[(index + 1) % keyPoints.Count];
+
+        Handles.DrawLine(from, to);
+    }
+
+    void DrawKeyPoint(int index)
+    {
+        Handles.DotCap(0, keyPoints[index], Quaternion.identity, HandleUtility.GetHandleSize(keyPoints[index]) * 0.03f);
+    }
+
+    int NearestLine(out Vector3 position)
+    {
+        var near = -1;
+        var nearDist = float.MaxValue;
+        position = keyPoints[0];
+
+        for (int i = 0; i < keyPoints.Count; i++)
+        {
+
+            var j = (i + 1) % keyPoints.Count;
+
+            Vector2 nearPos;
+            var distance = CGAlgorithm.PointToSegementDistance(mousePosition, keyPoints[i], keyPoints[j], out nearPos);
+
+            if (distance < nearDist)
+            {
+                nearDist = distance;
+                position = nearPos;
+                near = i;
+            }
+
+        }
+        return near;
+    }
+    
+    bool TryHoverSegment(out int index)
+    {
+        if(TryHover(keyPoints, Color.white, out index))
+        {
+            mouseCorsor = MouseCursor.MoveArrow;
+            return true;
+        }
+        return false;
+    }
+
+    bool TryHover(List<Vector3> points, Color color, out int index)
+    {
+        if(Tools.current == Tool.Move)
+        {
+            index = NearestLine(out nearestPosition);
+            if(index >= 0 && IsSegmentHovering(nearestPosition))
+            {
+                Handles.color = color;
+                DrawCircle(nearestPosition, clickRadius);
+                return true;
+            }
+            
+        }
+        index = -1;
+        return false;
+    }
+
+    bool IsSegmentHovering(Vector3 position)
+    {
+        return Vector3.Distance(mousePosition, position) < HandleUtility.GetHandleSize(position) * clickRadius;
+
+    }
+
+    bool TryDragSegment(List<Vector3> points, int index)
+    {
+        if (e.type == EventType.MouseDown && IsSegmentHovering(nearestPosition))
+        {
+            clickPosition = mousePosition;
+            return true;
+        }
+        return false;
+    }
 }
--- Assets/V1.0 BasicBox/Scripts/PackageImporter/PanelImporter.cs
+++ Assets/V1.0 BasicBox/Scripts/PackageImporter/PanelImporter.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Geometry;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
-public class PanelImporter : MonoBehaviour {
+public class PanelImporter : MonoBehaviour
+{
 
 
 
@@ -17,6 +19,9 @@ public class PanelImporter : MonoBehaviour {
     /// </summary>
     public int Col { set; get; }
 
+    public float Width { get { return BorderRect.size.x; } }
+    public float Height { get { return BorderRect.size.y; } }
+
     //public RectOffset Border { set; get; }
     public float Left { set; get; }
     public float Right { set; get; }
@@ -28,10 +33,11 @@ public class PanelImporter : MonoBehaviour {
     public List<CreaseData> Creases { set; get; }
 
     private Shape2D _shape;
-    public Shape2D Shape {
+    public Shape2D Shape
+    {
         get
         {
-            if(_shape == null)
+            if (_shape == null)
             {
                 _shape = new Shape2D();
                 _shape.AddMesh(GetComponent<MeshFilter>().sharedMesh);
@@ -40,16 +46,55 @@ public class PanelImporter : MonoBehaviour {
         }
     }
 
-    
-    
+    public List<Vector2> Outline
+    {
+        get
+        {
 
-    /*// Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}*/
+            return Shape.OutlinePoints.Select(p => (Vector2)p.Position).ToList();
+
+        }
+    }
+
+    public List<Vector2> Bleedline
+    {
+        get
+        {
+            return CGAlgorithm.ScalePoly(Outline, 0.03f);
+        }
+    }
+
+    public Rect BorderRect
+    {
+        get
+        {
+            var min_x = Outline.Min(p => p.x);
+            var max_x = Outline.Max(p => p.x);
+            var min_y = Outline.Min(p => p.y);
+            var max_y = Outline.Max(p => p.y);
+
+            return Rect.MinMaxRect(min_x, min_y, max_x, max_y);
+
+        }
+    }
+
+    public Vector3[] BorderArray
+    {
+        get
+        {
+            var min_x = Outline.Min(p => p.x);
+            var max_x = Outline.Max(p => p.x);
+            var min_y = Outline.Min(p => p.y);
+            var max_y = Outline.Max(p => p.y);
+
+            return new Vector3[]
+            {
+                new Vector2(min_x, min_y),
+                new Vector2(min_x, max_y),
+                new Vector2(max_x, max_y),
+                new Vector2(max_x, min_y)
+            };
+        }
+    }
+
 }
