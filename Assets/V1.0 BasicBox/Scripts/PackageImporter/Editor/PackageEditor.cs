e9ddfbec0cdfb78ce603ac275b6f7236fa601f80
Robin <impression.cs@gmail.com>
2016-04-15 14:58:38 +0800

    Change SrcMatrix from [,] to CartesianMatrix

--- Assets/V1.0 BasicBox/Scripts/PackageImporter/Editor/PackageEditor.cs
+++ Assets/V1.0 BasicBox/Scripts/PackageImporter/Editor/PackageEditor.cs
@@ -31,13 +31,28 @@ public class PackageEditor : Editor
 
         //DrawLine(ImPackage.Outline, new Color(0, 0.5f, 0.8f));
 
-        DrawLine(mPackage.Bleedline, new Color(0.0f, 0.4f, 0.6f));
+        _HandlesHelper.DrawLine(mPackage.Bleedline, new Color(0.0f, 0.4f, 0.6f));
 
         foreach (Transform child in mPackage.transform)
         {
             var childPanel = child.GetComponent<Panel>();
-            DrawLine(childPanel.destVertices, new Color(0, 1, 0), 4);
+            _HandlesHelper.DrawLine(childPanel.destVertices, new Color(0, 1, 0), 4);
         }
+
+        /*for (int row = -1; row <= 2; row++)
+        {
+            for (int col = -3; col <= 3; col++)
+            {
+                var rect = mPackage.SrcCarMatrix[row, col];
+                _HandlesHelper.DrawLine(rect.Vector2Array(), new Color(0, 1, 0), 4);
+                Handles.Label(rect.center, "[" + row + ", " + col + "]\n" + rect.ToString());
+            }
+        }*/
+
+        /*foreach(var face in mPackage.Shape.Faces)
+        {
+            DrawLine(face.BasicPoints.Select(p =>(Vector2)p.Position), new Color(0, 1, 0), 4);
+        }*/
 
         HandleUtility.Repaint();
 
@@ -59,66 +74,5 @@ public class PackageEditor : Editor
         get { return EditorPrefs.GetBool("PackageEditor_dimensionSettings", false); }
         set { EditorPrefs.SetBool("PackageEditor_dimensionSettings", value); }
     }
-
-
-    void DrawKeyPoints()
-    {
-        int i = 0;
-        foreach (var p in mPackage.Shape.Points)
-        {
-            //Handles.DotCap(0, p, Quaternion.identity, HandleUtility.GetHandleSize(p) * 0.03f);
-            Handles.Label(p.Position, "" + i++);
-        }
-
-
-    }
-
-    void DrawBleedLine()
-    {
-
-        var outline = mPackage.Shape.OutlinePoints.Select(p => (Vector2)p.Position).ToList();
-        var bleedline = CGAlgorithm.ScalePoly(outline, 0.0125f);
-
-        DrawLine(bleedline, Color.blue);
-
-    }
-
-    void DrawLine(IEnumerable<Vector2> points, Color32 color, float DottedSpaceSize = 0)
-    {
-        if (points == null || points.Count() == 0)
-            return;
-
-        Handles.color = color;
-
-        //var first = points.First();
-        //var end = points.Last();
-
-        var current = points.Last();
-
-        foreach (var next in points)
-        {
-            if (DottedSpaceSize > 0)
-                Handles.DrawDottedLine(current, next, DottedSpaceSize);
-            else
-                Handles.DrawLine(current, next);
-
-            current = next;
-        }
-
-        //Handles.DrawLine(end, first);
-    }
-
-    /*void AddMesh(Mesh mesh)
-    {
-        
-        for (int i = 0; i < mesh.triangles.Length; i += 3)
-        {
-
-            var p0 = new Point2D(mesh.vertices[mesh.triangles[i]]);
-            var p1 = new Point2D(mesh.vertices[mesh.triangles[i + 1]]);
-            var p2 = new Point2D(mesh.vertices[mesh.triangles[i + 2]]);
-
-            var face = Shape.AddPoints(p0, p1, p2);
-        }
-    }*/
+    
 }
--- Assets/V1.0 BasicBox/Scripts/PackageImporter/Package.cs
+++ Assets/V1.0 BasicBox/Scripts/PackageImporter/Package.cs
@@ -8,59 +8,31 @@ using System.Collections;
 //[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class Package : MonoBehaviour
 {
-
-
-
-    //public GameObject InitMesh { set; get; }
-
-    //public List<PanelData> Panels = new List<PanelData>();
-
-    //public List<CreaseData> Creases = new List<CreaseData>();
-    
-    //public List<Vector3> Vertices { set; get; }
-
-    //public List<int> Indices { set; get; }
-
     public Shape2D Shape { set; get; }
 
     public List<Vector2> Outline { set; get; }
 
     public List<Vector2> Bleedline { set; get; }
 
-    //public List<Dimension> Dimensions { set; get; }
-
-    //public Vector3 InitDim { set; get; }
-
     public float Length { set; get; }
     public float Width { set; get; }
     public float Depth { set; get; }
     public float Thickness { set; get; }
 
-    public Rect[,] RectMatrix { set; get; }
+    //public Rect[,] srcMatrix { set; get; }
 
     public List<Crease> Creases { set; get; }
 
     //public Panel[,] Panels { set; get; }
-    public Matrix<Panel> Panels { set; get; }
+    public CartesianMatrix<Panel> Panels { set; get; }
 
-    #region Resize
+    public CartesianMatrix<Rect> SrcCarMatrix { set; get; }
 
-    public Rect[,] ResizeMatrix { set; get; }
+    public Rect[,] destMatrix { set; get; }
 
     public void OnResize(float length, float width, float depth, float thickness = 0.01f)
     {
-        ResizeMatrix = CalcRectMatrix(length, width, depth);
-
-        /*foreach(Transform child in transform)
-        {
-            var panel = child.GetComponent<Panel>();
-            if (!panel)
-            {
-                //panel = child.gameObject.AddComponent<PanelImporter>();
-                continue;
-            }
-            panel.OnResize();
-        }*/
+        destMatrix = CalcRectMatrix(length, width, depth);
 
         for (int row = -2, i = 0; row <= 2; row++, i++)
         {
@@ -70,15 +42,13 @@ public class Package : MonoBehaviour
                 if (panel == null)
                     continue;
 
-                var offsetMin = ResizeMatrix[i, j].min - RectMatrix[i, j].min;
-                var offsetMax = ResizeMatrix[i, j].max - RectMatrix[i, j].max;
+                var offsetMin = destMatrix[i, j].min - SrcCarMatrix[row, col].min;//srcMatrix[i, j].min;//
+                var offsetMax = destMatrix[i, j].max - SrcCarMatrix[row, col].max; //srcMatrix[i, j].max;// 
                 panel.OnResize(offsetMin, offsetMax);
             }
         }
 
     }
-
-    #endregion
 
     public void OnInit(float length, float width, float depth, float thickness = 0.01f)
     {
@@ -86,7 +56,7 @@ public class Package : MonoBehaviour
         InitDimension(length, width, depth, thickness);
 
         InitShape();
-        
+
     }
 
     void InitDimension(float length, float width, float depth, float thickness)
@@ -96,13 +66,23 @@ public class Package : MonoBehaviour
         Depth = depth;
         Thickness = thickness;
 
-        RectMatrix = CalcRectMatrix(length, width, depth);
-        ResizeMatrix = CalcRectMatrix(length, width, depth);
+        //srcMatrix = CalcRectMatrix(length, width, depth);
+        SrcCarMatrix = CalcCarMatrix(length, width, depth);
+
+        /*var row = -1;
+        for (int col = -3; col <= 3; col++)
+        {
+            var rect = SrcCarMatrix[row, col];
+            SrcCarMatrix[row, col] = Rect.MinMaxRect(rect.xMin, rect.yMin - (rect.width - rect.height), rect.xMax, rect.yMax);
+        }*/
+
+        ///
+        destMatrix = CalcRectMatrix(length, width, depth);
     }
 
     void InitShape()
     {
-        Panels = new Matrix<Panel>();
+        Panels = new CartesianMatrix<Panel>(5, 7);
 
         Shape = new Shape2D();
 
@@ -110,8 +90,8 @@ public class Package : MonoBehaviour
 
         foreach (Transform child in transform)
         {
-            var mesh = child.GetComponent<MeshFilter>().sharedMesh;
-            Shape.AddMesh(mesh);
+            /*var mesh = child.GetComponent<MeshFilter>().sharedMesh;
+            Shape.AddMesh(mesh);*/
 
             var panel = child.GetComponent<Panel>();
             if (!panel)
@@ -120,7 +100,7 @@ public class Package : MonoBehaviour
             }
 
             //panel.OnInit(this);
-            panel.mPackage = this;
+
             panel.InitShape();
             Add(panel);
 
@@ -146,7 +126,7 @@ public class Package : MonoBehaviour
             for (int col = -3, j = 0; col <= 3; col++, j++)
             {
                 var c_x = col * d_x;
-                var c_y = row * (col % 2 == 0 ? d_y_0 : d_y_1)+ 0.0059F;
+                var c_y = row * (col % 2 == 0 ? d_y_0 : d_y_1);// + 0.0059F;
 
                 var w = col % 2 == 0 ? length : width;
                 var h = row % 2 == 0 ? depth : (col % 2 == 0 ? width : length);
@@ -155,7 +135,7 @@ public class Package : MonoBehaviour
                 var max_x = c_x + w * 0.5f;
                 var min_y = c_y - h * 0.5f;
                 var max_y = c_y + h * 0.5f;
-                
+
                 //result[i, j] = Rect.MinMaxRect(min_x, min_y, max_x, max_y);
                 result[i, j] = new Rect(min_x, min_y, w, h);
             }
@@ -164,15 +144,49 @@ public class Package : MonoBehaviour
         return result;
     }
 
+    public CartesianMatrix<Rect> CalcCarMatrix(float length, float width, float depth)
+    {
+        CartesianMatrix<Rect> result = new CartesianMatrix<Rect>(5, 7);
+
+        var d_x = (length + width) * 0.5f;
+        var d_y_0 = (depth + width) * 0.5f;
+        var d_y_1 = (depth + length) * 0.5f;
+
+        for (int row = -2; row <= 2; row++)
+        {
+            for (int col = -3; col <= 3; col++)
+            {
+                var c_x = col * d_x;
+                var c_y = row * (col % 2 == 0 ? d_y_0 : d_y_1);// + 0.0059F;
+
+                var w = col % 2 == 0 ? length : width;
+                var h = row % 2 == 0 ? depth : (col % 2 == 0 ? width : length);
+
+                var min_x = c_x - w * 0.5f;
+                var max_x = c_x + w * 0.5f;
+                var min_y = c_y - h * 0.5f;
+                var max_y = c_y + h * 0.5f;
+
+                //result[i, j] = Rect.MinMaxRect(min_x, min_y, max_x, max_y);
+                result[row, col] = new Rect(min_x, min_y, w, h);
+            }
+        }
+
+        return result;
+    }
+
     public void Add(Panel panel)
     {
-        
+
+        panel.mPackage = this;
+        panel.Face = Shape.AddPoints(panel.Outline.Select(p => new Point2D(p)).ToArray());
 
         for (int row = -2, i = 0; row <= 2; row++, i++)
         {
             for (int col = -3, j = 0; col <= 3; col++, j++)
             {
-                var rect = RectMatrix[i, j];
+                //var rect = srcMatrix[i, j];
+                var rect = SrcCarMatrix[row, col];
                 //var vertices = rect.Vector2Array();
                 if (CGAlgorithm.CN_PnPoly(panel.Center, rect.Vector2Array()) == 1)
                 {
@@ -186,48 +200,26 @@ public class Package : MonoBehaviour
         throw new System.Exception("NO SUCH RECTANGLE ");
     }
 
-    public Rect GetDimVertices(Vector2 position, ref int r, ref int c)
-    {
-
-        for (int row = -2, i = 0; row <= 2; row++, i++)
-        {
-            for (int col = -3, j = 0; col <= 3; col++, j++)
-            {
-                var rect = RectMatrix[i, j];
-                //var vertices = rect.Vector2Array();
-                if (CGAlgorithm.CN_PnPoly(position, rect.Vector2Array()) == 1)
-                {
-                    r = row;
-                    c = col;
-                    return rect;
-                }
-            }
-        }
-        
-        throw new System.Exception("NO SUCH RECTANGLE ");
-    }
-
-    public void GetRectTransformByRowAndCol(int row, int col, ref Vector2 offsetMin, ref Vector2 offsetMax)
-    {
-        if(ResizeMatrix == null || RectMatrix == null)
-        {
-            return;
-        }
-
-        var i = row + 2;
-        var j = col + 3;
-
-        offsetMin = ResizeMatrix[i, j].min - RectMatrix[i, j].min;
-        offsetMax = ResizeMatrix[i,j].max - RectMatrix[i, j].max;
-        ///return new RectOffset(max.x, min.x, max.y, min.y);
-    }
 }
-public class Matrix<T>
-{
-    private const int offsetRow = 2;
-    private const int offsetCol = 3;
 
-    private T[,] mPanels = new T[5, 7];
+/// <summary>
+/// Cartesian coordinate system
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class CartesianMatrix<T>
+{
+    private int offsetRow;// = 2;
+    private int offsetCol;//= 3;
+
+    private T[,] mPanels;// = new T[5, 7];
+
+    public CartesianMatrix(int rowNum, int colNum)
+    {
+        offsetRow = rowNum / 2;
+        offsetCol = colNum / 2;
+        mPanels = new T[rowNum, colNum];
+    }
+
     public T this[int row, int col]
     {
         set { mPanels[row + offsetRow, col + offsetCol] = value; }
