1e56a902521598f13d84a099b69278bf228b3702
Robin <impression.cs@gmail.com>
2016-04-23 00:05:34 +0800

    use System.Linq

--- Assets/Utils/Serializable/SerializableExtensions.cs
+++ Assets/Utils/Serializable/SerializableExtensions.cs
@@ -6,53 +6,29 @@ public static class SerializableExtensions
 {
     public static SerializableVector2[] ToSerializableVector2Array(this List<Vector2> listVector2)
     {
-        List<SerializableVector2> result = new List<SerializableVector2>();
-        foreach (var pt in listVector2.Select(p => new SerializableVector2(p.x, p.y)))
-        {
-            result.Add(pt);
-        }
-        return result.ToArray();
+        return listVector2.Select(p => (SerializableVector2)p).ToArray();
     }
 
     public static SerializableVector2[] ToSerializableVector2Array(this Vector2[] listVector2)
     {
-        List<SerializableVector2> result = new List<SerializableVector2>();
-        foreach (var pt in listVector2.Select(p => new SerializableVector2(p.x, p.y)))
-        {
-            result.Add(pt);
-        }
-        return result.ToArray();
+        return listVector2.Select(p => (SerializableVector2)p).ToArray();
     }
 
-    public static SerializableVector3[] ToSerializableVector3Array(this Vector3[] listVector2)
+    public static SerializableVector3[] ToSerializableVector3Array(this Vector3[] listVector3)
     {
-        List<SerializableVector3> result = new List<SerializableVector3>();
-        foreach (var pt in listVector2.Select(p => new SerializableVector3(p.x, p.y, p.z)))
-        {
-            result.Add(pt);
-        }
-        return result.ToArray();
+        
+        return listVector3.Select(p => (SerializableVector3)p).ToArray();
     }
 
     public static Vector3[] ToVector3Array(this SerializableVector3[] src)
 
     {
-        Vector3[] result = new Vector3[src.Length];
-        for (int i = 0; i < src.Length; i++)
-        {
-            result[i] = (Vector3)src[i];
-        }
-        return result;
+        return src.Select(p => (Vector3)p).ToArray();
     }
 
     public static Vector2[] ToVector2Array(this SerializableVector2[] src)
 
     {
-        Vector2[] result = new Vector2[src.Length];
-        for (int i = 0; i < src.Length; i++)
-        {
-            result[i] = (Vector2)src[i];
-        }
-        return result;
+        return src.Select(p => (Vector2)p).ToArray();
     }
 }
