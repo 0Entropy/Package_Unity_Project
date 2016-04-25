e9e7c3d2d3e03281e8a05dd7a26dcac6eed4a3ff
Robin <impression.cs@gmail.com>
2016-04-25 16:19:21 +0800

    Box and Renderer Context

---
+++ Assets/V1.0 BasicBox/Scripts/BoxMeshBuilder/Box.cs
@@ -0,0 +1,274 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using Geometry;
+using System;
+
+/// <summary>
+/// It is a basic package class.
+/// </summary>
+public class Box
+{
+
+    public float Length { set; get; }
+    public float Width { set; get; }
+    public float Depth { set; get; }
+    public float Thickness { set; get; }
+
+    public List<Face> Faces { set; get; }
+
+    public CartesianMatrix<Rect> SrcCartMatrix { set; get; }
+    public CartesianMatrix<Rect> DestCartMatrix { set; get; }
+
+    public Rect Boundry
+    {
+        get
+        {
+            float min_x = 0;
+            float min_y = 0;
+            float max_x = 0;
+            float max_y = 0;
+            foreach (var face in Faces)
+            {
+                min_x = Mathf.Min(min_x, face.Bleedline.Min(p => p.x));
+                max_x = Mathf.Max(max_x, face.Bleedline.Max(p => p.x));
+                min_y = Mathf.Min(min_y, face.Bleedline.Min(p => p.y));
+                max_y = Mathf.Max(max_y, face.Bleedline.Max(p => p.y));
+            }
+
+            return Rect.MinMaxRect(min_x, min_y, max_x, max_y);
+        }
+    }
+
+    public Face this[int row, int col]
+    {
+        get { return Faces.Find(p => (p.Row == row && p.Col == col)); }
+    }
+
+    public void OnResize(float length, float width, float depth, float thickness = 0.01f)
+    {
+        DestCartMatrix = CalcCartMatrix(length, width, depth);
+
+        foreach (var face in Faces)
+        {
+            face.Object3D.transform.position = Vector3.zero;
+            face.Object3D.transform.rotation = Quaternion.identity;
+
+            face.UpdateDimension(DestCartMatrix[face.Row, face.Col]);
+        }
+
+        foreach (var edge in Edges)
+            edge.IsRotated = false;
+
+    }
+
+    #region Generable Box
+    public void OnInit(PackageData data)
+    {
+
+        InitDimension(data.Length, data.Width, data.Depth, data.Thickness);
+
+        InitFaces(data.Panels);
+
+        InitEdges(data.Creases);
+
+    }
+
+    #endregion
+
+    #region Face
+
+    public void InitFaces(List<PanelData> datas)
+    {
+
+        Faces = new List<Face>();
+
+        foreach (var data in datas)
+        {
+            var face = new Face();
+            face.OnInit(data);
+
+            face.mBox = this;
+
+            Faces.Add(face);
+
+        }
+
+    }
+
+    #endregion
+
+    #region Edge
+
+    public bool IsNotBinded { get { return Edges.Any(e => !e.IsBinded); } }
+
+    public List<Edge> Edges;
+    private void InitEdges(List<CreaseData> creases)
+    {
+        Edges = new List<Edge>();
+        foreach (var crease in creases)
+        {
+            var edge = new Edge(crease.Vertices[0], crease.Vertices[1], crease.FoldAngle);
+            foreach (var neighbor in crease.Neighbors)
+            {
+                edge.AddFace(this[neighbor[0], neighbor[1]]);
+            }
+            Edges.Add(edge);
+        }
+    }
+
+    #endregion
+
+    void InitDimension(float length, float width, float depth, float thickness)
+    {
+        Length = length;
+        Width = width;
+        Depth = depth;
+        Thickness = thickness;
+
+        SrcCartMatrix = CalcCartMatrix(length, width, depth);
+        DestCartMatrix = CalcCartMatrix(length, width, depth);
+    }
+
+    public void OnBindFaces(GameObject obj)
+    {
+        if (IsNotBinded)
+        {
+
+            var root = Faces.Find(f => f.IsRoot);
+            root.Object3D.transform.SetParent(obj.transform);
+            BindByEdge(root);
+        }
+    }
+
+    void BindByEdge(Face face)
+    {
+        foreach (var f in face.Neighbors)
+        {
+            var edge = f.FindEdgeByFace(face);
+            if (!edge.IsBinded)
+            {
+                f.Object3D.transform.SetParent(face.Object3D.transform);
+                edge.IsBinded = true;
+                BindByEdge(f);
+            }
+        }
+    }
+
+    public void OnRotateFaces()
+    {
+        while (Faces.FindAll(f => f.IsNext).Count > 0)
+        {
+            RotateAroundEdge();
+        }
+    }
+
+    void RotateAroundEdge()
+    {
+        foreach (var f in Faces.FindAll(f => f.IsNext).Distinct())
+        {
+            //Debug.Log(face.mTransform.name);
+            var edge = f.Edges.Find(e => !e.IsRotated);
+
+            var point = f.Translate(edge.Point);
+
+            f.Object3D.transform.RotateAround(point, edge.Axis, edge.Angle);
+            edge.IsRotated = true;
+            //RotateByEdge(f);
+        }
+    }
+    
+        public void Draw2D(GameObject obj, Material mater)
+        {
+            foreach (var face in Faces)
+            {
+                if (!face.Object2D)
+                {
+                    face.Object2D = new GameObject(string.Format("[{0},{1}]", face.Row, face.Col));
+                    face.Object2D.transform.SetParent(obj.transform);
+                }
+
+                var face2D = face.Object2D.GetComponent<Mesh3DRenderer>();
+
+                if (face2D == null)
+                {
+                    face2D = face.Object2D.AddComponent<Mesh3DRenderer>();
+                    face2D.FaceType = Facing.FACE_BACK;
+                    face2D.IsTinkness = false;
+                    face2D.tinkness = 0.0F;
+                    face2D.GetComponent<MeshRenderer>().material = mater;
+                }
+
+                face2D.Clear();
+                face2D.OuterPolygon.Add(face.Bleedline);
+                face2D.UVRect = Boundry;
+                face2D.UpdateMesh();
+            }
+
+        }
+
+        public void Draw3D(GameObject obj, Material mater)
+        {
+            foreach (var face in Faces)
+            {
+                if (!face.Object3D)
+                {
+                    face.Object3D = new GameObject(string.Format("[{0},{1}]", face.Row, face.Col));
+
+                }
+
+                var face3D = face.Object3D.GetComponent<Mesh3DRenderer>();
+
+                if (face3D == null)
+                {
+                    face3D = face.Object3D.AddComponent<Mesh3DRenderer>();
+                    face3D.FaceType = Facing.FACE_BACK;
+                    face3D.IsTinkness = false;
+                    face3D.tinkness = 0.016F;
+                    face3D.GetComponent<MeshRenderer>().material = mater;
+                }
+
+                face3D.Clear();
+                face3D.OuterPolygon.Add(face.DestOutline);
+                face3D.UVRect = Boundry;
+                face3D.UpdateMesh();
+            }
+            
+
+            OnBindFaces(obj);
+
+            OnRotateFaces();
+        }
+
+    public static CartesianMatrix<Rect> CalcCartMatrix(float length, float width, float depth)
+    {
+        CartesianMatrix<Rect> result = new CartesianMatrix<Rect>(5, 7);
+
+        var d_x = (length + width) * 0.5f;
+        var d_y_0 = (depth + width) * 0.5f;
+        var d_y_1 = (depth + length) * 0.5f;
+
+        for (int row = -2; row <= 2; row++)
+        {
+            for (int col = -3; col <= 3; col++)
+            {
+                var c_x = col * d_x;
+                var c_y = row * (col % 2 == 0 ? d_y_0 : d_y_1);
+
+                var w = col % 2 == 0 ? length : width;
+                var h = row % 2 == 0 ? depth : (col % 2 == 0 ? width : length);
+
+                var min_x = c_x - w * 0.5f;
+                var max_x = c_x + w * 0.5f;
+                var min_y = c_y - h * 0.5f;
+                var max_y = c_y + h * 0.5f;
+
+                result[row, col] = new Rect(min_x, min_y, w, h);
+            }
+        }
+
+        return result;
+    }
+
+}
+
---
+++ Assets/V1.0 BasicBox/Scripts/BoxMeshBuilder/BoxContext.cs
@@ -0,0 +1,197 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System.Collections.Generic;
+
+public class BoxContext : MonoBehaviour
+{
+
+    public TextAsset data;
+    public Material mater;
+    
+    public GameObject box2D;
+    public GameObject box3D;
+    
+    Box mBox;
+    
+    void Start()
+    {
+
+
+        mBox = new Box();
+        var package = JsonFx.Json.JsonReader.Deserialize<PackageData>(data.text);
+        //var package = JsonUtility.FromJson<PackageData>(data.text);
+
+        mBox.OnInit(package);
+
+        inputL.text = mBox.Length.ToString();
+        inputW.text = mBox.Width.ToString();
+        inputD.text = mBox.Depth.ToString();
+
+        mBox.Draw3D(box3D, mater);
+        
+        mBox.Draw2D(box2D, mater);
+
+        box2D.transform.Rotate(Vector3.up, 180);
+    }
+
+
+    public void OnResize(float length, float width, float depth, float thickness = 0.01f)
+    {
+
+        Length = length;
+        Width = width;
+        Depth = depth;
+
+        Debug.Log(string.Format("[{0}, {1}, {2}]", length, width, depth));
+        mBox.OnResize(length, width, depth, thickness);
+
+        /* Draw3D();
+
+         Draw2D();*/
+        mBox.Draw3D(box3D, mater);
+
+        mBox.Draw2D(box2D, mater);
+
+    }
+    
+    #region 2D  
+
+    void Draw2D()
+    {
+        foreach (var face in mBox.Faces)
+        {
+            if (!face.Object2D)
+            {
+                face.Object2D = new GameObject(string.Format("[{0},{1}]", face.Row, face.Col));
+                face.Object2D.transform.SetParent(box2D.transform);
+            }
+
+            var face2D = face.Object2D.GetComponent<Mesh3DRenderer>();
+
+            if (face2D == null)
+            {
+                face2D = face.Object2D.AddComponent<Mesh3DRenderer>();
+                face2D.FaceType = Facing.FACE_BACK;
+                face2D.IsTinkness = false;
+                face2D.tinkness = 0.0F;
+                face2D.GetComponent<MeshRenderer>().material = mater;
+            }
+
+            face2D.Clear();
+            face2D.OuterPolygon.Add(face.Bleedline);
+            face2D.UVRect = mBox.Boundry;
+            face2D.UpdateMesh();
+        }
+
+    }
+    #endregion
+    void Draw3D()
+    {
+        foreach (var face in mBox.Faces)
+        {
+            if (!face.Object3D)
+            {
+                face.Object3D = new GameObject(string.Format("[{0},{1}]", face.Row, face.Col));
+
+            }
+
+            var face3D = face.Object3D.GetComponent<Mesh3DRenderer>();
+
+            if (face3D == null)
+            {
+                face3D = face.Object3D.AddComponent<Mesh3DRenderer>();
+                face3D.FaceType = Facing.DOUBLE_FACE;
+                face3D.IsTinkness = true;
+                face3D.tinkness = 0.016F;
+                face3D.GetComponent<MeshRenderer>().material = mater;
+            }
+
+            face3D.Clear();
+            face3D.OuterPolygon.Add(face.DestOutline);
+            face3D.UVRect = mBox.Boundry;
+            face3D.UpdateMesh();
+        }
+        
+        mBox.OnBindFaces(box3D);
+        
+        mBox.OnRotateFaces();
+    }
+
+    /*void BindByEdge(Face face)
+    {
+        foreach (var f in face.Neighbors)
+        {
+            var edge = f.FindEdgeByFace(face);
+            if (!edge.IsBinded)
+            {
+                f.Object3D.transform.SetParent(face.Object3D.transform);
+                edge.IsBinded = true;
+                BindByEdge(f);
+            }
+        }
+    }*/
+
+    void RotateAroundEdge()
+    {
+        foreach (var f in mBox.Faces.FindAll(f => f.IsNext).Distinct())
+        {
+            //Debug.Log(face.mTransform.name);
+            var edge = f.Edges.Find(e => !e.IsRotated);
+
+            var point = f.Translate(edge.Point);
+
+            f.Object3D.transform.RotateAround(point, edge.Axis, edge.Angle);
+            edge.IsRotated = true;
+            //RotateByEdge(f);
+        }
+    }
+
+    #region Unit Test
+
+    float Length, Width, Depth;
+    // Use this for initialization
+    public InputField inputL, inputW, inputD;
+
+    public void OnRefresh()
+    {
+        OnResize(Length, Width, Depth);
+    }
+
+    public void OnRandom()
+    {
+        Length = ((int)(Random.Range(1.0F, 3.0F) * 100)) * 0.01F;
+        Width = ((int)(Random.Range(1.0F, 3.0F) * 100)) * 0.01F;
+        Depth = ((int)(Random.Range(1.0F, 3.0F) * 100)) * 0.01F;
+
+        OnResize(Length, Width, Depth);
+    }
+
+    public void PulsLength()
+    {
+        Length += 0.05F;
+        OnResize(Length, Width, Depth);
+    }
+
+    public void PlusWidth()
+    {
+        Width += 0.05F;
+        OnResize(Length, Width, Depth);
+    }
+
+    public void PlusDepth()
+    {
+        Depth += 0.05F;
+        OnResize(Length, Width, Depth);
+    }
+
+    public void OnResize()
+    {
+        Length = float.Parse(inputL.text);
+        Width = float.Parse(inputW.text);
+        Depth = float.Parse(inputD.text);
+        OnResize(Length, Width, Depth);
+
+    }
+    #endregion
+}
